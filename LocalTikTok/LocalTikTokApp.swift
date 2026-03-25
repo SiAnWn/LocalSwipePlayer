2f9a2f97e2049256ee50ed1a711d38972dd647ba
SiAnWn <116614425+SiAnWn@users.noreply.github.com>
2026-03-25 14:44:48 +0800

    Update LocalTikTokApp.swift

--- LocalTikTok/LocalTikTokApp.swift
+++ LocalTikTok/LocalTikTokApp.swift
@@ -2,9 +2,12 @@ import SwiftUI
 
 @main
 struct LocalTikTokApp: App {
+    @StateObject private var videoModel = VideoModel()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(videoModel)
         }
     }
 }
